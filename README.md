77595d325314c26bff9de79b1340a4ce0db1b5cb
hpnog <joao.nogueira01@gmail.com>
2019-12-10 23:43:39 +0000

    Two Sum Problem Solution Added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# sePrep
+# Software Enineering Prep
+
+## LeetCode Exercise Solutions
+
+ * [Two-Sum](https://leetcode.com/problems/two-sum/) - 10/12/2019;
+ *
---
+++ leetCodeSolutions/src/twoSum.cpp
@@ -0,0 +1,18 @@
+# Found in https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int,int> haveSeenIt;
+        for(int i = 0; i < nums.size(); i++) {
+            if(haveSeenIt.find(target - nums[i]) != haveSeenIt.end()) {
+                res.push_back(haveSeenIt[target - nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            haveSeenIt.insert(pair<int, int>(nums[i], i));
+        }
+        return res;
+    }
+};
