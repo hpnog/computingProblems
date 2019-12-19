75f7c959ceffc2cf47b315c56ab41b6135fc4732
João Nogueira <joao.nogueira@fabamaq.com>
2019-12-19 15:11:45 +0000

    Fix and remove warnings

--- src/leetCodeSolutions/zigzagConversion.cpp
+++ src/leetCodeSolutions/zigzagConversion.cpp
@@ -10,11 +10,11 @@ std::string SolutionZigZagConversion::convert(std::string s, int numRows) {
         for(int i = 0; i < zigZagReps && result.size() < s.size(); i++) {
             int lineBase = repSize * i;
 
-            if((lineBase + line) < s.size())
+            if((uint) (lineBase + line) < s.size())
                 result += s[lineBase + line];
             
             int lineSpecific = lineBase + repSize - line;
-            if(line > 0 && line < (numRows - 1) && lineSpecific < s.size()) {
+            if((uint) line > 0 && line < (numRows - 1) && (uint) lineSpecific < s.size()) {
                 result += s[lineSpecific];
             }
         }
@@ -37,6 +37,7 @@ int SolutionZigZagConversion::runTestCases() {
     std::cout << "Selecting Zig Zag Conversion Probem...\n";
     std::cout << "Going to run test cases:\n\n";
 
+    std::cout << "Reverse of: " << testCase_1 << " is expected: " << solution_1 << " | Result is: " << convert(testCase_1, numRows_1) << std::endl;
     std::cout << "Reverse of: " << testCase_2 << " is expected: " << solution_2 << " | Result is: " << convert(testCase_2, numRows_2) << std::endl;
  
     return 0;
