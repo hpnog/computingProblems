f16b25be8fa21737d90d111a2996c75f05c4703f
hpnog <joao.nogueira01@gmail.com>
2019-12-29 00:32:59 +0000

    project initialized

---
+++ leetCodeSolutionsKotlin/src/test/kotlin/IntToRomanTest.kt
@@ -0,0 +1,17 @@
+import leetCodeSolutionsKotlin.intToRoman.Solution
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class IntToRomanTest {
+
+    private val solution = Solution()
+
+    @Test
+    fun intToRoman() {
+        assertEquals("III", solution.intToRoman(3))
+        assertEquals("IV", solution.intToRoman(4))
+        assertEquals("IX", solution.intToRoman(9))
+        assertEquals("LVIII", solution.intToRoman(58))
+        assertEquals("MCMXCIV", solution.intToRoman(1994))
+    }
+}
