ff4eb93e3cbac1182c6b60ca01ba401b4ff9c162
hpnog <joao.nogueira01@gmail.com>
2019-12-29 15:33:07 +0000

    Attempt 2

--- leetCodeSolutionsKotlin/build.gradle.kts
+++ leetCodeSolutionsKotlin/build.gradle.kts
@@ -1,7 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    application
     kotlin("jvm") version "1.3.61"
     jacoco
 }
@@ -11,7 +8,9 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
 }
 
@@ -21,35 +20,13 @@ jacoco {
 }
 
 tasks.jacocoTestReport {
-        reports {
-                xml.isEnabled = true
-                csv.isEnabled = true
-                html.isEnabled = true
-        }
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = true
+        html.isEnabled = true
+    }
 }
 
 tasks.test {
     finalizedBy(tasks.jacocoTestReport)
-
-    testLogging {
-        events("passed", "failed", "skipped")
-
-    }
-    useJUnitPlatform()
-}
-
-val compileKotlin: KotlinCompile by tasks
-val compileTestKotlin: KotlinCompile by tasks
-
-tasks.withType<KotlinCompile> {
-
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-        apiVersion = "1.3"
-        languageVersion = "1.3"
-        allWarningsAsErrors = true
-    }
 }
